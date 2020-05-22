d952c394a827801f5820b8900556441b3cbe57d1
Michael VanOverbeek <theplexgate@gmail.com>
2020-05-22 15:20:36 -0400

    update navigation menus

--- components/wtf-navigation.vue
+++ components/wtf-navigation.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <v-tabs v-if="$menu('primary')" dark color="white" background-color="transparent">
-      <v-tab v-for="item of $menu('primary')" :key="item.name" :href="item.href" :to="item.to">
+      <v-tab
+        v-for="item of $menu('primary')"
+        :key="item._id"
+        :href="item.type === 'external' ? item.href : null"
+        :to="item.type !== 'external' ? item.href : null"
+      >
         {{ item.name }}
       </v-tab>
     </v-tabs>
